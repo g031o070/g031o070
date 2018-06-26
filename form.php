e9689c5f928f67e87d1fc2f3b964baaa27bc4e61
g031o070 <39939713+g031o070@users.noreply.github.com>
2018-06-26 12:54:23 +0900

    修正

--- form.php
+++ form.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbname = "mysql:host=localhost;dbname=g031o070;charset=utf8";  //データベース、書式の設定
-$usrname = "test";  //ユーザー名
-$paword = "test";  //パスワード
+$dbname = "mysql:host=153.126.145.118;dbname=g031o070;charset=utf8";  //データベース、書式の設定
+$usrname = "g031o070";  //ユーザー名
+$paword = "g031o070";  //パスワード
 
 try{
   $db = new PDO($dbname, $usrname, $paword, array(PDO::ATTR_EMULATE_PREPARES => false));  //データベースに接続、型変換の回避
@@ -20,7 +20,7 @@ try{
 <body>
   <form action="record.php" method="post">
     ユーザー名：<input type="text" name="user_name" size="10" value="" /><br />
-    パスワード：<input type="text" name="user_password" size="10" value="" /><br />
+    パスワード：<input type="password" name="user_password" size="10" value="" /><br />
     <br />
     <input type="submit" value="追加する" />
   </form>
--- record.php
+++ record.php
@@ -1,8 +1,8 @@
 <?php
 
-$dbname = "mysql:host=localhost;dbname=g031o070;charset=utf8";  //データベース、書式の設定
-$usrname = "test";  //ユーザー名
-$paword = "test";  //パスワード
+$dbname = "mysql:host=153.126.145.118;dbname=g031o070;charset=utf8";  //データベース、書式の設定
+$usrname = "g031o070";  //ユーザー名
+$paword = "g031o070";  //パスワード
 
 try{
   $db = new PDO($dbname, $usrname, $paword, array(PDO::ATTR_EMULATE_PREPARES => false));  //データベースに接続、型変換の回避
@@ -10,14 +10,14 @@ try{
   $name = $_POST["user_name"];  //データの受け取り
   $password = $_POST["user_password"];  //データの受け取り
 
-  $stmt = $db -> prepare("INSERT INTO user (user_name, user_password) VALUES (:user_name, :user_password)");  //insert文
+  $stmt = $db -> prepare("INSERT INTO users (user_name, user_password) VALUES (:user_name, :user_password)");  //insert文
 
   //user_id(AUTO_INCREMENT)
   $stmt->bindParam(":user_name", $name, PDO::PARAM_STR);  //パラメータ、変数、型の指定
   $stmt->bindValue(":user_password", $password, PDO::PARAM_INT);  //パラメータ、変数、型の指定
   $stmt->execute();  //実行
 
-  $qry = "SELECT * FROM user";  //userテーブルの全レコードを取得
+  $qry = "SELECT * FROM users";  //userテーブルの全レコードを取得
   $data = $db -> query($qry);  //クエリの実行
 
   $db = null;  //データベースの切断
